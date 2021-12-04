633b71fca7aec986e082b60192a145bb79441cc9
Patrick Daguio <patrickdaguio@outlook.com>
2021-12-04 16:25:58 +0000

    Added PORTFOLIO section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+// Navbar Hide Scroll
+let lastScrollTop;
+let header = document.querySelector('.header');
+window.addEventListener('scroll', function () {
+	let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	if (scrollTop > lastScrollTop) {
+		header.classList.add('header-scroll');
+	} else {
+		header.classList.remove('header-scroll');
+	}
+
+	if (window.scrollY != 0) {
+		header.style.backgroundColor = 'rgba(23, 23, 23, 0.85)';
+		header.style.backdropFilter = 'blur(10px)';
+	} else {
+		header.style.backgroundColor = 'transparent';
+		header.style.backdropFilter = 'none';
+	}
+	lastScrollTop = scrollTop;
+});
