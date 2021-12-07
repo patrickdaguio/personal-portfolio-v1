1abf66f511c85a35516e1d022049461897a2884b
Patrick Daguio <patrickdaguio@outlook.com>
2021-12-07 20:30:20 +0000

    Added light and dark theme

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,10 @@
+// Navbar Active State
+const sections = document.querySelectorAll('section');
+const navbarLinks = document.querySelectorAll('.header__nav__list__link');
+
 // Navbar Hide Scroll
 let lastScrollTop;
-let header = document.querySelector('.header');
+const header = document.querySelector('.header');
 window.addEventListener('scroll', function () {
 	let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 	if (scrollTop > lastScrollTop) {
@@ -8,21 +12,38 @@ window.addEventListener('scroll', function () {
 	} else {
 		header.classList.remove('header-scroll');
 	}
+	if (window.scrollY != 0) header.classList.add('header-top');
+	else header.classList.remove('header-top');
 
-	if (window.scrollY != 0) {
-		header.style.backgroundColor = 'rgba(23, 23, 23, 0.85)';
-		header.style.backdropFilter = 'blur(10px)';
-	} else {
-		header.style.backgroundColor = 'transparent';
-		header.style.backdropFilter = 'none';
-	}
 	lastScrollTop = scrollTop;
+
+	let current = '';
+
+	sections.forEach(section => {
+		const sectionTop = section.offsetTop;
+		if (scrollY >= sectionTop - 550) {
+			current = section.getAttribute('id');
+		}
+	});
+
+	navbarLinks.forEach(link => {
+		link.classList.remove('header__nav__list__link--active');
+		if (link.classList.contains(current))
+			link.classList.add('header__nav__list__link--active');
+	});
 });
 
 // Theme Changer
-
-let themeBtn = document.querySelector('.theme-changer');
+const themeBtn = document.querySelector('.theme-changer');
+const body = document.querySelector('body');
 
 themeBtn.addEventListener('click', () => {
-	console.log(themeBtn);
+	if (body.classList[0] == 'theme--dark') {
+		body.style.backgroundColor = '#fff';
+		body.classList.replace('theme--dark', 'theme--light');
+	} else {
+		body.style.backgroundColor = '#171717';
+		body.classList.replace('theme--light', 'theme--dark');
+	}
+	body.classList.toggle('scroll-bar');
 });
